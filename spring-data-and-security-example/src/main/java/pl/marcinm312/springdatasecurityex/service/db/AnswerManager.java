60ce5d0f4bbe743eb05177a2661c836b1cc75fdf
Marcin Michalczyk <marcinm312@outlook.com>
2020-05-02 23:08:40 +0200

    Email notifications of published answers

--- spring-data-and-security-example/src/main/java/pl/marcinm312/springdatasecurityex/service/db/AnswerManager.java
+++ spring-data-and-security-example/src/main/java/pl/marcinm312/springdatasecurityex/service/db/AnswerManager.java
@@ -2,6 +2,8 @@ package pl.marcinm312.springdatasecurityex.service.db;
 
 import java.util.List;
 
+import javax.mail.MessagingException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,20 +11,25 @@ import pl.marcinm312.springdatasecurityex.enums.Roles;
 import pl.marcinm312.springdatasecurityex.exception.ChangeNotAllowedException;
 import pl.marcinm312.springdatasecurityex.exception.ResourceNotFoundException;
 import pl.marcinm312.springdatasecurityex.model.Answer;
+import pl.marcinm312.springdatasecurityex.model.Question;
 import pl.marcinm312.springdatasecurityex.model.User;
 import pl.marcinm312.springdatasecurityex.repository.AnswerRepository;
 import pl.marcinm312.springdatasecurityex.repository.QuestionRepository;
+import pl.marcinm312.springdatasecurityex.service.MailService;
 
 @Service
 public class AnswerManager {
 
 	private AnswerRepository answerRepository;
 	private QuestionRepository questionRepository;
+	private MailService mailService;
 
 	@Autowired
-	public AnswerManager(AnswerRepository answerRepository, QuestionRepository questionRepository) {
+	public AnswerManager(AnswerRepository answerRepository, QuestionRepository questionRepository,
+			MailService mailService) {
 		this.answerRepository = answerRepository;
 		this.questionRepository = questionRepository;
+		this.mailService = mailService;
 	}
 
 	public List<Answer> getAnswersByQuestionId(Long questionId) {
@@ -42,7 +49,17 @@ public class AnswerManager {
 		return questionRepository.findById(questionId).map(question -> {
 			answer.setQuestion(question);
 			answer.setUser(user);
-			return answerRepository.save(answer);
+			Answer savedAnswer = answerRepository.save(answer);
+			try {
+				String email = question.getUser().getEmail();
+				String subject = "Opublikowano odpowiedź na Twoje pytanie o id: " + question.getId();
+				String content = generateEmailContent(question, savedAnswer, true);
+				mailService.sendMail(email, subject, content, true);
+			} catch (MessagingException e) {
+				e.printStackTrace();
+			}
+			return savedAnswer;
+
 		}).orElseThrow(() -> new ResourceNotFoundException("Question not found with id " + questionId));
 	}
 
@@ -53,7 +70,17 @@ public class AnswerManager {
 		return answerRepository.findById(answerId).map(answer -> {
 			if (answer.getUser().getId() == user.getId() || user.getRole().equals(Roles.ROLE_ADMIN.name())) {
 				answer.setText(answerRequest.getText());
-				return answerRepository.save(answer);
+				Answer savedAnswer = answerRepository.save(answer);
+				try {
+					Question question = savedAnswer.getQuestion();
+					String email = question.getUser().getEmail();
+					String subject = "Zaktualizowano odpowiedź na Twoje pytanie o id: " + question.getId();
+					String content = generateEmailContent(question, savedAnswer, false);
+					mailService.sendMail(email, subject, content, true);
+				} catch (MessagingException e) {
+					e.printStackTrace();
+				}
+				return savedAnswer;
 			} else {
 				throw new ChangeNotAllowedException("Change not allowed!");
 			}
@@ -72,5 +99,23 @@ public class AnswerManager {
 				throw new ChangeNotAllowedException("Change not allowed!");
 			}
 		}).orElseThrow(() -> new ResourceNotFoundException("Answer not found with id " + answerId));
+	}
+
+	private String generateEmailContent(Question question, Answer answer, boolean isNewAnswer) {
+		User questionUser = question.getUser();
+		User answerUser = answer.getUser();
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("Witaj " + questionUser.getFirstName() + " " + questionUser.getLastName() + ",");
+		if (isNewAnswer) {
+			stringBuilder.append("<br/><br/>Użytkownik <b>" + answerUser.getUsername()
+					+ "</b> opublikował odpowiedź na Twoje pytanie:");
+		} else {
+			stringBuilder.append("<br/><br/>Użytkownik <b>" + answerUser.getUsername()
+					+ "</b> zaktualizował odpowiedź na Twoje pytanie:");
+		}
+		stringBuilder.append("<br/><br/><b>Tytuł:</b><br/>" + question.getTitle());
+		stringBuilder.append("<br/><br/><b>Opis:</b><br/>" + question.getDescription());
+		stringBuilder.append("<br/><br/><br/><b>Treść odpowiedzi:</b><br/>" + answer.getText().replace("\n", "<br/>"));
+		return stringBuilder.toString();
 	}
 }
