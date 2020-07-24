4f5653cf640c4a9dce77ac94efcfdefedc8739e9
Marcin Michalczyk <marcinm312@outlook.com>
2020-07-24 21:00:02 +0200

    Refactoring of security configuration and fix for bad credentials to API

--- spring-data-and-security-example/src/main/java/pl/marcinm312/springdatasecurityex/config/MultiHttpSecurityCustomConfig.java
+++ spring-data-and-security-example/src/main/java/pl/marcinm312/springdatasecurityex/config/MultiHttpSecurityCustomConfig.java
@@ -1,11 +1,9 @@
 package pl.marcinm312.springdatasecurityex.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,72 +14,54 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 
-import pl.marcinm312.springdatasecurityex.service.db.UserDetailsServiceImpl;
-
 @Configuration
 @EnableWebSecurity
-public class MultiHttpSecurityCustomConfig {
+public class MultiHttpSecurityCustomConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	@Configuration
-	@Order(1)
-	public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+    @Configuration
+    @Order(1)
+    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
 
-		private final UserDetailsServiceImpl userDetailsService;
+        protected void configure(HttpSecurity http) throws Exception {
+            http.
+                    antMatcher("/api/**").authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and().httpBasic()
+                    .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and().csrf().disable();
+        }
+    }
 
-		@Autowired
-		public ApiWebSecurityConfigurationAdapter(UserDetailsServiceImpl userDetailsService) {
-			this.userDetailsService = userDetailsService;
-		}
+    @Configuration
+    @Order(2)
+    public static class FormLoginWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
 
-		@Override
-		public void configure(AuthenticationManagerBuilder auth) throws Exception {
-			auth.userDetailsService(userDetailsService);
-		}
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .antMatcher("/**").authorizeRequests()
+                    .antMatchers("/", "/register", "/register/", "/token", "/token/", "/error", "error/").permitAll()
+                    .anyRequest().authenticated()
+                    .and().formLogin().permitAll()
+                    .and().logout().permitAll()
+                    .logoutSuccessUrl("/")
+                    .and().sessionManagement().maximumSessions(10000).maxSessionsPreventsLogin(false)
+                    .expiredUrl("/login").sessionRegistry(sessionRegistry());
+        }
 
-		protected void configure(HttpSecurity http) throws Exception {
-			http.antMatcher("/api/**").authorizeRequests().anyRequest().authenticated().and().httpBasic().and()
-					.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
-		}
-	}
+        @Bean
+        SessionRegistry sessionRegistry() {
+            return new SessionRegistryImpl();
+        }
 
-	@Configuration
-	@Order(2)
-	public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-
-		private final UserDetailsServiceImpl userDetailsService;
-
-		@Autowired
-		public FormLoginWebSecurityConfigurerAdapter(UserDetailsServiceImpl userDetailsService) {
-			this.userDetailsService = userDetailsService;
-		}
-
-		@Override
-		public void configure(AuthenticationManagerBuilder auth) throws Exception {
-			auth.userDetailsService(userDetailsService);
-		}
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.antMatcher("/**").authorizeRequests().antMatchers("/", "/register", "/register/", "/token", "/token/")
-					.permitAll().anyRequest().authenticated().and().formLogin().permitAll().and().logout().permitAll()
-					.logoutSuccessUrl("/").and().sessionManagement().maximumSessions(10000)
-					.maxSessionsPreventsLogin(false).expiredUrl("/login").sessionRegistry(sessionRegistry());
-		}
-
-		@Bean
-		SessionRegistry sessionRegistry() {
-			return new SessionRegistryImpl();
-		}
-
-		@Bean
-		public static ServletListenerRegistrationBean httpSessionEventPublisher() {
-			return new ServletListenerRegistrationBean(new HttpSessionEventPublisher());
-		}
-
-	}
+        @Bean
+        public static ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+            return new ServletListenerRegistrationBean<>(new HttpSessionEventPublisher());
+        }
+    }
 }
