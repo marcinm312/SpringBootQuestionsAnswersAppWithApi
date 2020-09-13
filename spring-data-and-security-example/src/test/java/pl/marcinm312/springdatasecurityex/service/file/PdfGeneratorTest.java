50219bcdcc5888dc6bc052fae7636fe9d804a9f2
Marcin Michalczyk <marcinm312@outlook.com>
2020-09-13 16:55:25 +0200

    Adding tests of PdfGenerator class

---
+++ spring-data-and-security-example/src/test/java/pl/marcinm312/springdatasecurityex/service/file/PdfGeneratorTest.java
@@ -0,0 +1,33 @@
+package pl.marcinm312.springdatasecurityex.service.file;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pl.marcinm312.springdatasecurityex.model.Question;
+import pl.marcinm312.springdatasecurityex.testdataprovider.QuestionDataProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class PdfGeneratorTest {
+
+    private static PdfGenerator pdfGenerator;
+
+    @BeforeAll
+    static void setup(){
+        pdfGenerator = new PdfGenerator();
+    }
+
+    @Test
+    void generateQuestionsPdfFile_simpleCase_success(){
+        List<Question> questionsList = QuestionDataProvider.prepareExampleQuestionsList();
+        Assertions.assertAll(() -> pdfGenerator.generateQuestionsPdfFile(questionsList));
+        Assertions.assertDoesNotThrow(() -> pdfGenerator.generateQuestionsPdfFile(questionsList));
+    }
+
+    @Test
+    void generateQuestionsPdfFile_emptyQuestionsList_success() {
+        Assertions.assertAll(() -> pdfGenerator.generateQuestionsPdfFile(new ArrayList<>()));
+        Assertions.assertDoesNotThrow(() -> pdfGenerator.generateQuestionsPdfFile(new ArrayList<>()));
+    }
+}
