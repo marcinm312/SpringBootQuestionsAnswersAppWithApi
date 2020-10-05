84029ac8c3ab7c663e4085139474704b2d2c9306
Marcin Michalczyk <marcinm312@outlook.com>
2020-10-06 00:43:45 +0200

    Security update to FirstUserCreator class

--- spring-data-and-security-example/src/main/java/pl/marcinm312/springdatasecurityex/service/db/FirstUserCreator.java
+++ spring-data-and-security-example/src/main/java/pl/marcinm312/springdatasecurityex/service/db/FirstUserCreator.java
@@ -2,12 +2,11 @@ package pl.marcinm312.springdatasecurityex.service.db;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import pl.marcinm312.springdatasecurityex.enums.Roles;
 import pl.marcinm312.springdatasecurityex.model.User;
 import pl.marcinm312.springdatasecurityex.repository.UserRepo;
@@ -15,27 +14,25 @@ import pl.marcinm312.springdatasecurityex.repository.UserRepo;
 @Service
 public class FirstUserCreator {
 
-	@Value("${admin.default.password}")
-	private String password;
-
-	@Value("${admin.default.email}")
-	private String email;
-
 	private final UserRepo userRepo;
 	private final PasswordEncoder passwordEncoder;
+	private final Environment environment;
 
 	protected final org.slf4j.Logger log = LoggerFactory.getLogger(getClass());
 
 	@Autowired
-	public FirstUserCreator(UserRepo userRepo, PasswordEncoder passwordEncoder) {
+	public FirstUserCreator(UserRepo userRepo, PasswordEncoder passwordEncoder, Environment environment) {
 		this.userRepo = userRepo;
 		this.passwordEncoder = passwordEncoder;
+		this.environment = environment;
 	}
 
 	@EventListener(ApplicationReadyEvent.class)
 	public User addFirstUser() {
 		String login = "administrator";
 		if (!userRepo.findByUsername(login).isPresent()) {
+			String password = environment.getProperty("admin.default.password");
+			String email = environment.getProperty("admin.default.email");
 			User user = new User();
 			user.setUsername(login);
 			user.setPassword(passwordEncoder.encode(password));
