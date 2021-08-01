b9c309e995350bd3fdf8ddf3ffe6a22b1949488a
marcinm312 <marcin.michalczyk.312@gmail.com>
2021-08-01 21:09:37 +0200

    Remove unnecessary validations

--- spring-data-and-security-example/src/main/java/pl/marcinm312/springdatasecurityex/validator/UserCreateValidator.java
+++ spring-data-and-security-example/src/main/java/pl/marcinm312/springdatasecurityex/validator/UserCreateValidator.java
@@ -14,12 +14,10 @@ import java.util.Optional;
 public class UserCreateValidator implements Validator {
 
 	private static final String USERNAME_FIELD = "username";
-	private static final String PASSWORD_FIELD = "password";
 	private static final String CONFIRM_PASSWORD_FIELD = "confirmPassword";
 
 	private static final String USER_EXISTS_ERROR = "user_exists_error";
 	private static final String CONFIRM_PASSWORD_ERROR = "confirm_password_error";
-	private static final String START_END_SPACE_ERROR = "start_end_space_error";
 
 	private final UserManager userManager;
 
@@ -37,18 +35,10 @@ public class UserCreateValidator implements Validator {
 	public void validate(Object target, Errors errors) {
 		UserCreate user = (UserCreate) target;
 
+		String username = user.getUsername();
 		String password = user.getPassword();
 		String confirmPassword = user.getConfirmPassword();
 
-		if (password.startsWith(" ") || password.endsWith(" ")) {
-			errors.rejectValue(PASSWORD_FIELD, START_END_SPACE_ERROR, "Hasło nie może się zaczynać lub kończyć spacją");
-		}
-
-		if (confirmPassword.startsWith(" ") || confirmPassword.endsWith(" ")) {
-			errors.rejectValue(CONFIRM_PASSWORD_FIELD, START_END_SPACE_ERROR, "Hasło nie może się zaczynać lub kończyć spacją");
-		}
-
-		String username = user.getUsername();
 		Optional<User> foundUser = userManager.findUserByUsername(username);
 		if (foundUser.isPresent()) {
 			errors.rejectValue(USERNAME_FIELD, USER_EXISTS_ERROR, "Użytkownik o takim loginie już istnieje!");
