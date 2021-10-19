f70f7fbadafa4feee8fcab5049311898c4f6925b
marcinm312 <marcin.michalczyk.312@gmail.com>
2021-10-19 20:45:51 +0200

    Improvement of exception handling

--- spring-data-and-security-example/src/main/java/pl/marcinm312/springdatasecurityex/config/security/jwt/JsonObjectAuthenticationFilter.java
+++ spring-data-and-security-example/src/main/java/pl/marcinm312/springdatasecurityex/config/security/jwt/JsonObjectAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package pl.marcinm312.springdatasecurityex.config.security.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -16,12 +17,15 @@ public class JsonObjectAuthenticationFilter extends UsernamePasswordAuthenticati
 
 	private final ObjectMapper objectMapper;
 
+	private final org.slf4j.Logger log = LoggerFactory.getLogger(getClass());
+
 	public JsonObjectAuthenticationFilter(ObjectMapper objectMapper) {
 		this.objectMapper = objectMapper;
 	}
 
 	@Override
-	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
 
 		try {
 			BufferedReader reader = request.getReader();
@@ -37,7 +41,14 @@ public class JsonObjectAuthenticationFilter extends UsernamePasswordAuthenticati
 			setDetails(request, token);
 			return this.getAuthenticationManager().authenticate(token);
 		} catch (IOException e) {
-			throw new IllegalArgumentException(e.getMessage());
+			log.error("Error while authenticating user: {}", e.getClass().getName());
+			try {
+				response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+			} catch (IOException ex) {
+				log.error("Error while sending error: {}", ex.getMessage());
+				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			}
+			return null;
 		}
 	}
 }
