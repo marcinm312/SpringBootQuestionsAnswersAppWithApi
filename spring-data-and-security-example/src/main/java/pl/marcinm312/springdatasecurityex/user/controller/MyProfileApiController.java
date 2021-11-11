f9140ea0522044983e6ab7d52df158835194acd8
marcinm312 <marcin.michalczyk.312@gmail.com>
2021-11-11 21:02:34 +0100

    User data update API

--- spring-data-and-security-example/src/main/java/pl/marcinm312/springdatasecurityex/user/controller/MyProfileApiController.java
+++ spring-data-and-security-example/src/main/java/pl/marcinm312/springdatasecurityex/user/controller/MyProfileApiController.java
@@ -2,25 +2,46 @@ package pl.marcinm312.springdatasecurityex.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import pl.marcinm312.springdatasecurityex.user.model.dto.UserDataUpdate;
 import pl.marcinm312.springdatasecurityex.user.model.dto.UserGet;
 import pl.marcinm312.springdatasecurityex.user.service.UserManager;
+import pl.marcinm312.springdatasecurityex.user.validator.UserDataUpdateValidator;
 
 @RestController
 @RequestMapping("/api/myProfile")
 public class MyProfileApiController {
 
 	private final UserManager userManager;
+	private final UserDataUpdateValidator userDataUpdateValidator;
 
 	@Autowired
-	public MyProfileApiController(UserManager userManager) {
+	public MyProfileApiController(UserManager userManager, UserDataUpdateValidator userDataUpdateValidator) {
 		this.userManager = userManager;
+		this.userDataUpdateValidator = userDataUpdateValidator;
+	}
+
+	@InitBinder("userDataUpdate")
+	private void initBinder(WebDataBinder binder) {
+		binder.addValidators(userDataUpdateValidator);
 	}
 
 	@GetMapping
 	public UserGet getMyProfile(Authentication authentication) {
 		return userManager.getUserDTOByAuthentication(authentication);
+	}
+
+	@PutMapping("/update")
+	public UserGet updateMyProfile(@Validated @RequestBody UserDataUpdate user, BindingResult bindingResult,
+								   Authentication authentication) throws BindException {
+		if (bindingResult.hasErrors()) {
+			throw new BindException(bindingResult);
+		} else {
+			return userManager.updateUserData(user, authentication);
+		}
 	}
 }
