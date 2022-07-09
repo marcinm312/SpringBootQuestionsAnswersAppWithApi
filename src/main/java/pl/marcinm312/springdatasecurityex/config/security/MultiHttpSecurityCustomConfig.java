d727a6f93da6e98af4f640853e54d0e9ba26b28c
marcinm312 <marcin.michalczyk.312@gmail.com>
2022-07-09 21:10:14 +0200

    Dependencies update and new Spring Security configuration

--- src/main/java/pl/marcinm312/springdatasecurityex/config/security/MultiHttpSecurityCustomConfig.java
+++ src/main/java/pl/marcinm312/springdatasecurityex/config/security/MultiHttpSecurityCustomConfig.java
@@ -7,15 +7,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import pl.marcinm312.springdatasecurityex.config.security.jwt.*;
@@ -23,7 +24,7 @@ import pl.marcinm312.springdatasecurityex.user.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
-public class MultiHttpSecurityCustomConfig extends WebSecurityConfigurerAdapter {
+public class MultiHttpSecurityCustomConfig {
 
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
@@ -32,33 +33,30 @@ public class MultiHttpSecurityCustomConfig extends WebSecurityConfigurerAdapter 
 
 	@Configuration
 	@Order(1)
-	public class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+	public static class ApiWebSecurityConfigurationAdapter {
 
 		private final UserDetailsServiceImpl userDetailsService;
 		private final ObjectMapper objectMapper;
 		private final RestAuthenticationSuccessHandler successHandler;
 		private final RestAuthenticationFailureHandler failureHandler;
 		private final Environment environment;
+		private final AuthenticationConfiguration authenticationConfiguration;
 
 		@Autowired
 		public ApiWebSecurityConfigurationAdapter(UserDetailsServiceImpl userDetailsService, ObjectMapper objectMapper,
 												  RestAuthenticationSuccessHandler successHandler,
 												  RestAuthenticationFailureHandler failureHandler,
-												  Environment environment) {
+												  Environment environment, AuthenticationConfiguration authenticationConfiguration) {
 			this.userDetailsService = userDetailsService;
 			this.objectMapper = objectMapper;
 			this.successHandler = successHandler;
 			this.failureHandler = failureHandler;
 			this.environment = environment;
+			this.authenticationConfiguration = authenticationConfiguration;
 		}
 
-		@Override
-		public void configure(AuthenticationManagerBuilder auth) throws Exception {
-			auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
-		}
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
+		@Bean
+		public SecurityFilterChain jwtFilterChain(HttpSecurity http) throws Exception {
 			http.antMatcher("/api/**")
 					.authorizeRequests().antMatchers(
 							"/api/login", "/api/registration", "/api/token"
@@ -66,9 +64,15 @@ public class MultiHttpSecurityCustomConfig extends WebSecurityConfigurerAdapter 
 					.anyRequest().authenticated()
 					.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 					.and().addFilter(authenticationFilter())
-					.addFilter(new JwtAuthorizationFilter(authenticationManager(), userDetailsService, environment))
+					.addFilter(new JwtAuthorizationFilter(authenticationManager(authenticationConfiguration), userDetailsService, environment))
 					.exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
 					.and().csrf().disable();
+			return http.build();
+		}
+
+		@Bean
+		public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+			return authenticationConfiguration.getAuthenticationManager();
 		}
 
 		public JsonObjectAuthenticationFilter authenticationFilter() throws Exception {
@@ -76,7 +80,7 @@ public class MultiHttpSecurityCustomConfig extends WebSecurityConfigurerAdapter 
 			authenticationFilter.setFilterProcessesUrl("/api/login");
 			authenticationFilter.setAuthenticationSuccessHandler(successHandler);
 			authenticationFilter.setAuthenticationFailureHandler(failureHandler);
-			authenticationFilter.setAuthenticationManager(super.authenticationManager());
+			authenticationFilter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
 			return authenticationFilter;
 		}
 
@@ -84,22 +88,10 @@ public class MultiHttpSecurityCustomConfig extends WebSecurityConfigurerAdapter 
 
 	@Configuration
 	@Order(2)
-	public class FormLoginWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+	public static class FormLoginWebSecurityConfigurationAdapter {
 
-		private final UserDetailsServiceImpl userDetailsService;
-
-		@Autowired
-		public FormLoginWebSecurityConfigurationAdapter(UserDetailsServiceImpl userDetailsService) {
-			this.userDetailsService = userDetailsService;
-		}
-
-		@Override
-		public void configure(AuthenticationManagerBuilder auth) throws Exception {
-			auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
-		}
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
+		@Bean
+		public SecurityFilterChain formLoginFilterChain(HttpSecurity http) throws Exception {
 			http.antMatcher("/**")
 					.authorizeRequests().antMatchers(
 							"/", "/register", "/register/", "/token", "/token/", "/error", "error/",
@@ -110,6 +102,7 @@ public class MultiHttpSecurityCustomConfig extends WebSecurityConfigurerAdapter 
 					.and().formLogin().loginPage("/loginPage").loginProcessingUrl("/authenticate").permitAll()
 					.and().logout().permitAll().logoutSuccessUrl("/").logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 					.and().sessionManagement().maximumSessions(10000).maxSessionsPreventsLogin(false).expiredUrl("/loginPage").sessionRegistry(sessionRegistry());
+			return http.build();
 		}
 
 		@Bean
