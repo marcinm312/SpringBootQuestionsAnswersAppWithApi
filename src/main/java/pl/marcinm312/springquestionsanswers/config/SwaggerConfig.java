c9483ffd1b32f7f3f7f4fa5f4036b420b770cc03
marcinm312 <marcin.michalczyk.312@gmail.com>
2023-03-12 00:00:00 +0100

    Adding actuator and Swagger upgrade

--- src/main/java/pl/marcinm312/springquestionsanswers/config/SwaggerConfig.java
+++ src/main/java/pl/marcinm312/springquestionsanswers/config/SwaggerConfig.java
@@ -1,50 +1,46 @@
 package pl.marcinm312.springquestionsanswers.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.Authentication;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.service.*;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
-	private ApiKey getApiKey() {
-		return new ApiKey("JWT", "Authorization", "header");
-	}
-
-	private SecurityContext securityContext() {
-
-		return SecurityContext.builder()
-				.securityReferences(defaultAuth())
-				.build();
-	}
-
-	private List<SecurityReference> defaultAuth() {
-
-		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-		authorizationScopes[0] = authorizationScope;
-		return List.of(new SecurityReference("JWT", authorizationScopes));
+	@Bean
+	public OpenAPI customizeOpenAPI() {
+		final String securitySchemeName = "bearerAuth";
+		return new OpenAPI()
+				.addSecurityItem(new SecurityRequirement()
+						.addList(securitySchemeName))
+				.components(new Components()
+						.addSecuritySchemes(securitySchemeName, new SecurityScheme()
+								.name(securitySchemeName)
+								.type(SecurityScheme.Type.HTTP)
+								.scheme("bearer")
+								.bearerFormat("JWT")
+								.description("login-api-controller -> /api/login")));
 	}
 
 	@Bean
-	public Docket api() {
+	public GroupedOpenApi publicApi() {
 
-		return new Docket(DocumentationType.SWAGGER_2)
-				.ignoredParameterTypes(Authentication.class)
-				.securityContexts(Collections.singletonList(securityContext()))
-				.securitySchemes(Collections.singletonList(getApiKey()))
-				.select()
-				.paths(PathSelectors.ant("/api/**"))
+		return GroupedOpenApi.builder()
+				.group("public-apis")
+				.pathsToMatch("/api/**")
+				.pathsToExclude("/api/actuator/**")
+				.build();
+	}
+
+	@Bean
+	public GroupedOpenApi actuatorApi() {
+		return GroupedOpenApi.builder()
+				.group("actuators")
+				.pathsToMatch("/api/actuator/**")
 				.build();
 	}
 }
