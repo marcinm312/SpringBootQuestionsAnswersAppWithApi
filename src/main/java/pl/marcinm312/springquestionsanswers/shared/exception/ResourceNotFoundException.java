e4a0126bbed584c533b2ed1bd47b4fd2481e829a
marcinm312 <marcin.michalczyk.312@gmail.com>
2023-03-12 21:12:01 +0100

    Logs improvements

--- src/main/java/pl/marcinm312/springquestionsanswers/shared/exception/ResourceNotFoundException.java
+++ src/main/java/pl/marcinm312/springquestionsanswers/shared/exception/ResourceNotFoundException.java
@@ -1,15 +1,18 @@
 package pl.marcinm312.springquestionsanswers.shared.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
 	public ResourceNotFoundException(String message) {
 		super(message);
+		log.error(getMessage());
 	}
 
 	public int getHttpStatus() {
--- src/main/java/pl/marcinm312/springquestionsanswers/user/exception/TokenNotFoundException.java
+++ src/main/java/pl/marcinm312/springquestionsanswers/user/exception/TokenNotFoundException.java
@@ -1,15 +1,18 @@
 package pl.marcinm312.springquestionsanswers.user.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class TokenNotFoundException extends RuntimeException {
 
 	public TokenNotFoundException() {
 		super("Nie znaleziono tokenu!");
+		log.error(getMessage());
 	}
 
 	public int getHttpStatus() {
