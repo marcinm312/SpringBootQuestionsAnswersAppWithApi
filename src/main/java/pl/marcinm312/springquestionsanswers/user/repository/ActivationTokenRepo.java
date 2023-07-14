34cfcc89a37c6e200b2cf38a3d1ddd34f3424747
marcinm312 <marcin.michalczyk.312@gmail.com>
2023-07-14 13:36:15 +0200

    Activation user DB improvement

--- src/main/java/pl/marcinm312/springquestionsanswers/user/repository/ActivationTokenRepo.java
+++ src/main/java/pl/marcinm312/springquestionsanswers/user/repository/ActivationTokenRepo.java
@@ -1,6 +1,8 @@
 package pl.marcinm312.springquestionsanswers.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.marcinm312.springquestionsanswers.user.model.ActivationTokenEntity;
 
@@ -9,5 +11,6 @@ import java.util.Optional;
 @Repository
 public interface ActivationTokenRepo extends JpaRepository<ActivationTokenEntity, Long> {
 
-	Optional<ActivationTokenEntity> findByValue(String value);
+	@Query("SELECT t FROM ActivationTokenEntity t LEFT JOIN FETCH t.user WHERE t.value = :value")
+	Optional<ActivationTokenEntity> findByValue(@Param("value") String value);
 }
