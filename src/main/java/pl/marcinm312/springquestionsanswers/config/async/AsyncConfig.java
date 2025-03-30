797dd6353f0b98251d323851156514196f4ddb4d
marcinm312 <marcin.michalczyk.312@gmail.com>
2025-03-30 22:58:10 +0200

    Virtual threads

--- src/main/java/pl/marcinm312/springquestionsanswers/config/async/AsyncConfig.java
+++ src/main/java/pl/marcinm312/springquestionsanswers/config/async/AsyncConfig.java
@@ -2,12 +2,13 @@ package pl.marcinm312.springquestionsanswers.config.async;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @Configuration
 @EnableScheduling
@@ -16,14 +17,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 public class AsyncConfig implements AsyncConfigurer {
 
 	@Bean(name = "mailExecutor")
-	public TaskExecutor getMailExecutor() {
-
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setMaxPoolSize(5);  // Maximum number of threads in the pool
-		executor.setThreadNamePrefix("MailExecutor-");  // Prefix for thread names
-		executor.setWaitForTasksToCompleteOnShutdown(true);  // Ensures tasks complete on shutdown
-		executor.setAwaitTerminationSeconds(60);  // Timeout for waiting for tasks to complete
-		executor.initialize();  // Initializes the thread pool
-		return executor;
+	public Executor getMailExecutor() {
+		return Executors.newVirtualThreadPerTaskExecutor();
 	}
 }
